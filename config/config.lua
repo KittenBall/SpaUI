6a62d5e01b17d96a3357074de231dfbf6b7eacd0
FitPanda <912967642@qq.com>
2020-11-24 09:02:16 +0800

    Update

--- config/config.lua
+++ config/config.lua
@@ -2,13 +2,28 @@ local addonName, SpaUI = ...
 
 local L = SpaUI.Localization
 
-local function CreateOptionsPanel(event, name)
-    if name ~= addonName then return end
-    local SpaUIOptionsPanel =
-        CreateFrame("Frame", "SpaUIOptionsPanel", UIParent)
-    SpaUIOptionsPanel.name = addonName
-    InterfaceOptions_AddCategory(SpaUIOptionsPanel)
-    return true
+local function CreateSpaUIButton()
+    local GameMenuButtonSpaUI = CreateFrame("BUTTON", "GameMenuButtonSpaUI",
+                                            GameMenuFrame,
+                                            "GameMenuButtonTemplate")
+    GameMenuButtonSpaUI:SetText(L["config_spaui_button"])
+    GameMenuButtonSpaUI:SetPoint("TOP", "GameMenuButtonWhatsNew", "BOTTOM", 0,
+                                 -1)
+    GameMenuButtonSpaUI:SetScript("OnClick", function(self)
+        PlaySound(SOUNDKIT.IG_MAINMENU_OPTION)
+    end)
 end
 
-SpaUI:RegisterEvent('ADDON_LOADED', CreateOptionsPanel)
+local function HookGameMenuFrameToAddSpaUIButton()
+    if not GameMenuButtonSpaUI then CreateSpaUIButton() end
+    GameMenuFrame:SetHeight(GameMenuFrame:GetHeight() +
+                                GameMenuButtonSpaUI:GetHeight() + 1)
+
+    local _, _, _, offX, offY = GameMenuButtonOptions:GetPoint()
+    GameMenuButtonOptions:ClearAllPoints()
+    GameMenuButtonOptions:SetPoint("TOP", GameMenuButtonSpaUI, "BOTTOM", offX,
+                                   offY)
+end
+
+hooksecurefunc('GameMenuFrame_UpdateVisibleButtons',
+               HookGameMenuFrameToAddSpaUIButton)
